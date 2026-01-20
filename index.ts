1a4c056e468a4282c0170db698d986920dc27902
Luseramia <108973001+Luseramia@users.noreply.github.com>
2026-01-20 23:28:46 +0700

    uncomment auth

--- index.ts
+++ index.ts
@@ -8,16 +8,16 @@ import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
   .use(ssoController)
-  // .onBeforeHandle(async (c) => {
-  //   if (!c.headers.authorization) {
-  //     c.set.status = 401;
-  //     return { error: "unauthorize" };
-  //   }
-  //   if (!(await tokenChecker(c.headers.authorization))) {
-  //     c.set.status = 403;
-  //     return { error: "Forbiden" };
-  //   }
-  // })
+  .onBeforeHandle(async (c) => {
+    if (!c.headers.authorization) {
+      c.set.status = 401;
+      return { error: "unauthorize" };
+    }
+    if (!(await tokenChecker(c.headers.authorization))) {
+      c.set.status = 403;
+      return { error: "Forbiden" };
+    }
+  })
   .use(ocrController)
   // .use(AuthorizationController)
   .use(
