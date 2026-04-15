2e923378de21ac9c0dcf04d433c350c21e692f57
Luseramia <108973001+Luseramia@users.noreply.github.com>
2026-04-15 14:07:06 +0700

    login reuse name

--- services/login/login.ts
+++ services/login/login.ts
@@ -1,11 +1,21 @@
 import dz from "../../drizzle.service";
 import { usersTable } from "../../db/users.schema";
 import pool from "../../pg-connector";
-
+import { eq, sql } from "drizzle-orm";
 export default class LoginService {
   async login(name: string) {
     try {
-      const user = await dz.insert(usersTable).values({name:name}).returning();
+      const isExist = await dz
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.name, name));
+      if(isExist){
+        return isExist[0];
+      }
+      const user = await dz
+        .insert(usersTable)
+        .values({ name: name })
+        .returning();
       return user[0];
     } catch (error) {
       console.log("error", error);
